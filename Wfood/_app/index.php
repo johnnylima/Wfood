7764d2eef91b47ffa68145810506dfdc5385a3a0
jolima <jolima@BR005C9180.getronics-latam.local>
2015-07-27 18:10:58 -0300

    CRUD e Config

---
+++ Wfood/_app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        $Dados = "";
+            $teste = new Create;
+            $teste->ExeCreate("usuario", $Dados)
+            
+        ?>
+    </body>
+</html>
